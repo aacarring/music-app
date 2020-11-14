24720202affcf5efebd96bf58726f9f0f2cf84ae
Alexa Carrington <acarring@wellesley.edu>
2020-11-14 12:26:39 -0500

    Added functions in js

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll('.sound');
     const pads = document.querySelectorAll('.pads div');
+
+    console.log(sounds[0]);
+
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function() {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+        })
+    })
 })
